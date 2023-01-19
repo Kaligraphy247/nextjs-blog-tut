4424940bb6a0898a851eb4204c737e979b24f57d
kaligraphy247 <chikeziejames3@gmail.com>
2023-01-19 10:46:56 +0100

    layout complete

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 <br>
-Used as starter for the  tutorial at [NextJS](https://nextjs.org/learn/foundations/about-nextjs).
+Used as starter for the  tutorial at [Next.js](https://nextjs.org/learn).
--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,65 @@
-// import * as styles from './layout.module.css';
-import styles from './layout.module.css'
+import * as styles from "./layout.module.css"; //* alternative
+// import styles from './layout.module.css' //* original
+import Head from "next/head";
+import Image from "next/image";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>;
+const name = "James";
+export const siteTitle = "Next.js Sample website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" /> {/* public is accessible? */}
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <Link href="/">
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={108}
+              width={108}
+              alt=""
+            />
+          </Link>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <>&larr;</> Back to Home
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
