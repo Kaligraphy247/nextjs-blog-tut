20b882022de8d45bc17afbbdfd2a6ce787ca6dc7
kaligraphy247 <chikeziejames3@gmail.com>
2023-01-19 11:22:05 +0100

    minor restructuring of 'first-post'

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 <br>
+
 Used as starter for the  tutorial at [Next.js](https://nextjs.org/learn).
+<br>
+
+To use TailwindCSS, click [Customizing PostCSS Config](https://nextjs.org/learn/basics/assets-metadata-css/styling-tips#:~:text=Customizing%20PostCSS%20Config) and [here](https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss) for a sample from Next.js and finally, click [here](https://tailwindcss.com/docs/guides/nextjs) to see recommended setup from TailwindCSS.
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,20 @@
 import Link from "next/link";
 import Head from "next/head";
 // import Script from "next/script"; //* unused import
-import Layout from "../components/layout";
-
+import Layout from "/components/layout";
 
 export default function FirstPost() {
-	return (
-		<Layout>
-			<Head>
-				<title>First Post</title>
-			</Head>
-			<h1>First Post</h1>
-			<h2>
-				<Link href="/">Back to home</Link>
-			</h2>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
 }
 
 //* load script before page becomes interactive
